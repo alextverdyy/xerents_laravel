772e3f0d326a173eda55d2470ff4b2adec72b72f
joaquinDaw2 <raposo.daw2@gmail.com>
2017-05-01 11:39:46 +0200

    extract event detail data

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -6,19 +6,26 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="panel panel-default">
-                        <div class="panel-heading"> <a href="/event/{{$event["@attributes"]["id"]}}">{{$event["title"]}}</a></div>
+                        <div class="panel-heading"> <a href="/event/{{$event["@attributes"]["id"]}}">{{$event["title"]}}</a>
+                            @foreach($event["categories"]["category"] as $category)
+                                <span class="label label-info">{{$category["name"]}}</span>
+                            @endforeach
+                        </div>
 
                             <div class="panel-body">
-                                Hora de inicio: {{$event["start_time"]}}<br>
-                                Lugar del evento: {{$event["venue_name"]}}<br>
-                                Ciudad: {{$event["city"]}},{{$event["country"]}}<br>
+                                <strong>Hora de inicio:</strong> {{$event["start_time"]}}<br>
+                                <strong>Lugar del evento:</strong> {{$event["venue_name"]}}<br>
+                                <strong>Ciudad:</strong> {{$event["city"]}},{{$event["country"]}}<br>
+                                <strong>Mapa:</strong><br>
+                                <img src="https://maps.googleapis.com/maps/api/staticmap?center={{$event["latitude"]}},{{$event["longitude"]}}&zoom=12&size=400x400&markers=color:blue%icon:%7Clabel:S%7C{{$event["latitude"]}},{{$event["longitude"]}}&key=AIzaSyA8pwDDJb4qzanENfLX9Xx5S0dn6YP8ino" alt="mapa"><br>
                             </div>
 
                             <div class="panel-body">
                                 OTROS DETALLES<br>
-                                <br> s
-                                Descripcion: {{$event["description"]}}<br>
-                                <a href="{{$event["url"]}}">Para mas informacion</a><br>
+                                <br>
+                                <strong>{{$event["title"]}}</strong><br>
+                                <strong>Descripcion:</strong> {{$event["description"]}}<br>
+                                <strong>Enlace:</strong> <a href="{{$event["url"]}}">Para mas informacion</a><br>
                             </div>
                     </div>
                 </div>
