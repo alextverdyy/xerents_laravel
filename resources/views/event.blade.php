926649276665e5c45661e41543c451a35d4ee56b
joaquinDaw2 <raposo.daw2@gmail.com>
2017-05-01 12:18:07 +0200

    Fix description event detail

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -7,9 +7,13 @@
                 <div class="col-md-8 col-md-offset-2">
                     <div class="panel panel-default">
                         <div class="panel-heading"> <a href="/event/{{$event["@attributes"]["id"]}}">{{$event["title"]}}</a>
+                            @if(in_array("categories",$event))
                             @foreach($event["categories"]["category"] as $category)
-                                <span class="label label-info">{{$category["name"]}}</span>
+                                    <span class="label label-info">{{$category["name"]}}</span>
                             @endforeach
+                            @else
+                                <span class="label label-info">No tiene categorias</span>
+                            @endif
                         </div>
 
                             <div class="panel-body">
@@ -24,7 +28,14 @@
                                 OTROS DETALLES<br>
                                 <br>
                                 <strong>{{$event["title"]}}</strong><br>
-                                <strong>Descripcion:</strong> {{$event["description"]}}<br>
+                                <?php
+                                if (is_string($event["description"])){
+                                    echo "<strong>Descripcion:</strong> <p class='more'>{{$event["description"]}}</p>";
+                                }else{
+                                    echo "<strong>Descripcion:</strong> No tiene descripcion";
+                                }
+                                ?>
+                                <br>
                                 <strong>Enlace:</strong> <a href="{{$event["url"]}}">Para mas informacion</a><br>
                             </div>
                     </div>
@@ -33,3 +44,9 @@
         </div>
 
 @endsection
+
+@if(is_string($category["name"]))
+    <span class="label label-info">{{$category["name"]}}</span>
+@else
+    <span class="label label-info">No tiene categorias</span>
+@endif
